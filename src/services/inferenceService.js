a1264678dad060a3c2faca99fb20db7983a3553a
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-10 15:46:53 +0700

    Update inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -2,7 +2,7 @@ const runInference = async (model, tensor) => {
     try {
         const predictions = model.predict(tensor).dataSync();
         const labelIndex = predictions.indexOf(Math.max(...predictions));
-        const labels = ['carrot', 'potato', 'broccoli']; // Sesuaikan dengan label model Anda
+        const labels = [cabbage', 'potato', 'cauliflower', 'Bottle_Gourd', 'Bean', 'Brinjal', 'Papaya', 'Pumpkin']; // Sesuaikan dengan label model Anda
         return [labels[labelIndex]];
     } catch (error) {
         throw new Error('Error during inference: ' + error.message);
