94bbf58ee4656acec230e01585fd59eba1cb7b2a
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-10 19:44:31 +0700

    Update storeData.js

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,13 +1,47 @@
-const { Firestore } = require('@google-cloud/firestore');
-const firestore = new Firestore();
+/** @format */
 
-const storeData = async (collection, data) => {
-    try {
-        await firestore.collection(collection).add(data);
-        console.log('Data stored successfully');
-    } catch (error) {
-        throw new Error('Error storing data: ' + error.message);
-    }
-};
+const { Firestore } = require("@google-cloud/firestore");
 
-module.exports = storeData;
+// Fungsi untuk memformat data dokumen Firestore
+function modelData(doc) {
+	return {
+		id: doc.id,
+		history: {
+			result: doc.data().result,
+			createdAt: doc.data().createdAt,
+			suggestion: doc.data().suggestion,
+			id: doc.id,
+		},
+	};
+}
+
+// Fungsi untuk menginisialisasi koneksi ke Firestore
+async function database() {
+	const settings = {
+		projectId: process.env.PROJECT_ID,
+	};
+	return new Firestore(process.env.APP_ENV === "local" ? settings : undefined);
+}
+
+// Fungsi untuk menyimpan data hasil pemindaian
+async function storeData(id, data) {
+	const ingredientCollection = (await database()).collection("ingredientScans"); // Nama koleksi diubah
+	return ingredientCollection.doc(id).set(data);
+}
+
+// Fungsi untuk mengambil data hasil pemindaian
+async function getData(id = null) {
+	const ingredientCollection = (await database()).collection("ingredientScans"); // Nama koleksi diubah
+	if (id) {
+		const doc = await ingredientCollection.doc(id).get();
+		if (!doc.exists) return null;
+		return modelData(doc);
+	} else {
+		const snapshot = await ingredientCollection.get();
+		const allData = [];
+		snapshot.forEach(doc => allData.push(modelData(doc)));
+		return allData;
+	}
+}
+
+module.exports = { storeData, getData, modelData };
