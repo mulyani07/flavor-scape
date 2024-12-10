7cdd1e7d4f323d5482d3defba3c0a0ca0920ea6a
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-10 19:28:28 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,13 +1,15 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require("@tensorflow/tfjs-node");
 
-const loadModel = async (modelPath) => {
-    try {
-        const model = await tf.loadLayersModel(`https://storage.googleapis.com/modelfv/models/model.json`);
-        console.log('Model loaded successfully');
-        return model;
-    } catch (error) {
-        throw new Error('Error loading model: ' + error.message);
-    }
-};
+async function loadModel() {
+	const modelPath = process.env.APP_ENV == "local" ? process.env.LOCAL_MODEL_URL : process.env.MODEL_URL;
+	console.log(Trying to load model from: ${modelPath});
+
+	try {
+		return await tf.loadGraphModel(modelPath);
+	} catch (error) {
+		console.error("Error loading model:", error);
+		throw error;
+	}
+}
 
 module.exports = loadModel;
