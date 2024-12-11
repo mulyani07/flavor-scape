ddb74b6f2169c7a21e3a2805807036db60a830a4
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-11 19:20:17 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const loadModel = require("../services/loadModel");
 
 const init = async () => {
     const server = Hapi.server({
         port: process.env.PORT || 8080,
-        host: 'localhost',
+         host: '0.0.0.0',
     });
+
+ try {
+        const model = await loadModel();
+        server.app.model = model;
+    } catch (error) {
+        console.error("Failed to load model:", error.message);
+        server.app.model = null; // Pastikan server tahu jika model gagal dimuat
+    }
 
     server.route(routes);
 
