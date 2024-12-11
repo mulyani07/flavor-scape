6f0fba06ba3603f5fd678d4503d47003800416b6
mulyani07 <153817975+mulyani07@users.noreply.github.com>
2024-12-11 19:19:01 +0700

    Update routes.js

--- src/server/routes.js
+++ src/server/routes.js
@@ -3,23 +3,23 @@
 const { postPredictHandler, getPredictHandler } = require("../server/handler");
 
 const routes = [
-	{
-		path: "/predict",
-		method: "POST",
-		handler: postPredictHandler,
-		options: {
-			payload: {
-				allow: "multipart/form-data",
-				multipart: true,
-			},
-		},
-	},
-	{
-		path: "/predict/histories",
-		method: "GET",
-		handler: getPredictHandler,
-		options: {},
-	},
+    {
+        path: "/predict",
+        method: "POST",
+        handler: postPredictHandler,
+        options: {
+            payload: {
+                allow: "multipart/form-data",
+                multipart: true,
+                maxBytes: 1048576 * 5, // Maksimum 5MB
+            },
+        },
+    },
+    {
+        path: "/predict/{id}",
+        method: "GET",
+        handler: getPredictHandler,
+    },
 ];
 
 module.exports = routes;
